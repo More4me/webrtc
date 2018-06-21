f2e8db0f40250ec792dfa921ef6824e1d0009d26
m.asfour <mennaasfour_fci2017@yahoo.com>
2018-06-21 14:03:30 +0200

    server side Done

--- test/chat.spec.js
+++ test/chat.spec.js
@@ -1,10 +1,19 @@
 const request = require('supertest');
 const { app } = require('../server');
+const { expect } =require('chai');
 describe('server', function () {
     it('should server html when a get request is made', function (done) {
         request(app)
             .get('/')
             .expect('Content-Type', 'text/html; charset=UTF-8')
             .expect(200, done);
+    });
+    it('should server the correct html page', function(done){
+        request(app)
+        .get('/')
+        .end((error, response)=> {
+            expect(response.text.match('<title>Cold Brew Tutorial</title>')).to.not.be.null;
+            done();
+        });
     });
 });
