bf11027fe9087f641e6ab3213ae8b2c08780cc8a
m.asfour <mennaasfour_fci2017@yahoo.com>
2018-06-24 10:33:16 +0200

    MOD:chat.spec.js and index.js to prevent reload
    using coldBrew (selenium)  we create chrome client and write tesxt and press enter + we faced issue with webdriver finally we downloaded driver 2.40

--- test/chat.spec.js
+++ test/chat.spec.js
@@ -1,6 +1,12 @@
 const request = require('supertest');
 const { app } = require('../server');
-const { expect } =require('chai');
+
+//to.not.be.null //http://www.chaijs.com/api/bdd/
+const { expect } = require('chai');
+
+const coldBrew = require('cold-brew');
+const { Key, By, Until } = require('selenium-webdriver');
+
 describe('server', function () {
     it('should server html when a get request is made', function (done) {
         request(app)
@@ -8,12 +14,47 @@ describe('server', function () {
             .expect('Content-Type', 'text/html; charset=UTF-8')
             .expect(200, done);
     });
-    it('should server the correct html page', function(done){
+
+    it('should server the correct html page', function (done) {
         request(app)
-        .get('/')
-        .end((error, response)=> {
-            expect(response.text.match('<title>Cold Brew Tutorial</title>')).to.not.be.null;
+            .get('/')
+            .end((error, response) => {
+                expect(response.text.match('<title>Cold Brew Tutorial</title>')).to.not.be.null;
+                done();
+            });
+    });
+});
+
+const PORT = 3000;
+const ADDRESS = `http://localhost:${PORT}/`;
+
+
+//cold-brew uses Selenium Webdriver behind the scenes
+describe('client-side messager application', function () {
+    let client;
+
+    //This creates an automated instance of Google Chrome
+    beforeEach(function () {
+        client = coldBrew.createClient();
+    });
+    it('should not reload the page when text form is submitted', function (done) {
+        this.timeout(10000);
+
+        client.get(ADDRESS);
+        client.do([
+            ['sendKeys', 'form input', { type: 'text' }, 'hello world' + Key.ENTER],
+            ['click', 'form button', {}],
+        ]);
+
+        client.executeScript(function () {
+            return window.location.href;
+        }).then((url) => {
+            expect(url).to.equal(ADDRESS);
             done();
         });
+    });
+
+    afterEach(function (done) {
+        client.quit().then(() => done());
     });
 });
